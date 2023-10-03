63c4af8d508a7a486c0ac557ed3541052313ff35
Shashvat Rawat <shashvatrawat729@gmail.com>
2023-10-04 01:11:21 +0530

    pretty much working

--- src/app/tetris/tetris.component.ts
+++ src/app/tetris/tetris.component.ts
@@ -48,12 +48,14 @@ export class TetrisComponent {
   ];
 
   horz: number = 4;
+  RESTART: boolean = false;
   randomShape: number = 0;
   randomColor: number = 0;
   Y_AXIS: number = 0;
   SPEED: number = 300;
+  SCORE: number = 0;
   PEAK_HEIGHT: number = 19;
-  currentBlockCoords: number[][] = [];
+  CURRENT_BLOCK: number[][] = [];
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
@@ -61,20 +63,27 @@ export class TetrisComponent {
     this.emptyTable = new Array(20)
       .fill(null)
       .map(() => new Array(10).fill(null));
-    this.funcArray = new Array(21).fill(null).map(() => new Array(10).fill(0));
-    for (let k = 0; k < 10; k++) {
-      this.funcArray[20][k] = 1;
-    }
+    this.cleanArray();
 
     this.startGame();
   }
 
+  cleanArray() {
+    this.funcArray = new Array(21).fill(null).map(() => new Array(10).fill(0));
+    for (let k = 0; k < 10; k++) {
+      this.funcArray[20][k] = 1;
+    }
+  }
+
   startGame() {
+    this.cleanArray();
+    this.PEAK_HEIGHT = 19;
     this.dropNewBlock();
   }
 
   lose() {
     console.log('lost');
+    this.SCORE = 0;
   }
 
   dropNewBlock() {
@@ -91,6 +100,10 @@ export class TetrisComponent {
 
   dropBlock() {
     const intervalId = setInterval(() => {
+      if (this.RESTART) {
+        this.RESTART = false;
+        clearInterval(intervalId);
+      }
       this.Y_AXIS++;
       // console.log(this.PEAK_HEIGHT);
 
@@ -111,7 +124,7 @@ export class TetrisComponent {
 
         this.colourCell(Y, X, this.COLOURS[this.randomColor]);
 
-        this.currentBlockCoords[k] = [Y, X];
+        this.CURRENT_BLOCK[k] = [Y, X];
       }
       if (this.checkMove('Down')) {
         clearInterval(intervalId);
@@ -125,33 +138,45 @@ export class TetrisComponent {
     }, this.SPEED);
   }
 
+  restart() {
+    this.RESTART = true;
+    this.SCORE = 0;
+    for (let k = 0; k < 4; k++) {
+      this.funcArray[this.CURRENT_BLOCK[k][0]][this.CURRENT_BLOCK[k][1]] =
+        this.randomColor;
+    }
+    for (let i = this.PEAK_HEIGHT; i <= 19; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (!this.isCoordEmpty(i, j)) {
+          this.colourCell(i, j, '#1a1a1a');
+        }
+      }
+    }
+    this.startGame();
+  }
+
   saveBlock() {
     console.log(this.funcArray);
     if (this.checkMove('Down')) {
       for (let k = 0; k < 4; k++) {
-        this.funcArray[this.currentBlockCoords[k][0]][
-          this.currentBlockCoords[k][1]
-        ] = this.randomColor;
-        const element = this.el.nativeElement.querySelector(
-          `#b${this.currentBlockCoords[k][0]}_${this.currentBlockCoords[k][1]}`
-        );
-        // this.renderer.setStyle(element, 'background-color', color);
+        this.funcArray[this.CURRENT_BLOCK[k][0]][this.CURRENT_BLOCK[k][1]] =
+          this.randomColor;
       }
       if (this.Y_AXIS < this.PEAK_HEIGHT) {
         this.PEAK_HEIGHT = this.Y_AXIS;
       }
-      setTimeout(() => {
+      if (this.rowClearCheck()) {
         this.dropNewBlock();
-      }, 100);
-      this.rowClear();
+      }
       return true;
     }
     return false;
   }
 
-  rowClear() {
+  rowClearCheck() {
     // console.log(this.SHAPES[this.randomShape][5]);
-
+    let hu = 0;
+    let rows_to_clear = [];
     for (let k = this.SHAPES[this.randomShape][5] - 1; k >= 0; k--) {
       let clearThis = true;
       for (let j = 0; j < 10 && clearThis; j++) {
@@ -161,24 +186,94 @@ export class TetrisComponent {
       }
 
       if (clearThis) {
-        for (let j = 0; j < 10; j++) {
-          this.colourCell(k + this.Y_AXIS, j, '#1a1a1a');
-          this.funcArray[k + this.Y_AXIS][j] = 0;
-        }
+        rows_to_clear.push(k + this.Y_AXIS + hu);
+        ++hu;
+      }
+    }
+    if (rows_to_clear.length == 0) {
+      return true;
+    } else {
+      this.rowSClear(rows_to_clear);
+      this.scoreAnim(rows_to_clear.length);
+      return false;
+    }
+  }
 
-        for (let j = k + this.Y_AXIS - 1; j >= this.PEAK_HEIGHT; j--) {
+  rowSClear(arr: number[]) {
+    let k = 1;
+    this.rowcll(arr[0]);
+    if (arr.length > 1) {
+      const intervalIdK = setInterval(() => {
+        this.rowcll(arr[k]);
+
+        ++k;
+        if (k == arr.length) {
+          clearInterval(intervalIdK);
+          this.dropNewBlock();
+        }
+      }, 1100);
+    } else {
+      setTimeout(() => {
+        this.dropNewBlock();
+      }, 1100);
+    }
+  }
+
+  scoreAnim(rows: number) {
+    let j = 0;
+    const intervalId = setInterval(() => {
+      const scoreEl = this.el.nativeElement.querySelector('#score');
+      this.renderer.addClass(scoreEl, 'score-animation-class');
+
+      setTimeout(() => {
+        this.renderer.removeClass(scoreEl, 'score-animation-class');
+      }, 150);
+      ++j;
+      if (j == 10) {
+        clearInterval(intervalId);
+      }
+    }, 100);
+  }
+
+  rowcll(row_to_clear: number) {
+    let j = 0;
+    const intervalId = setInterval(() => {
+      console.log(j);
+      this.SCORE++;
+      const element = this.el.nativeElement.querySelector(
+        `#b${row_to_clear}_${j}`
+      );
+      if (j > 0) {
+        const elementPrev = this.el.nativeElement.querySelector(
+          `#b${row_to_clear}_${j - 1}`
+        );
+        this.renderer.removeClass(elementPrev, 'burst-animation-class');
+        this.colourCell(row_to_clear, j - 1, '#1a1a1a');
+      }
+
+      this.renderer.addClass(element, 'burst-animation-class');
+      this.funcArray[row_to_clear][j] = 0;
+
+      ++j;
+      if (j == 10) {
+        clearInterval(intervalId);
+        const elementPrev = this.el.nativeElement.querySelector(
+          `#b${row_to_clear}_${j - 1}`
+        );
+        this.renderer.removeClass(elementPrev, 'burst-animation-class');
+        this.colourCell(row_to_clear, j - 1, '#1a1a1a');
+        for (let y = row_to_clear - 1; y >= this.PEAK_HEIGHT; y--) {
           for (let h = 0; h < 10; h++) {
-            if (!this.isCoordEmpty(j, h)) {
-              this.colourCell(j, h, '#1a1a1a');
-              this.colourCell(j + 1, h, this.COLOURS[this.funcArray[j][h]]);
-              this.funcArray[j + 1][h] = this.funcArray[j][h];
-              this.funcArray[j][h] = 0;
+            if (!this.isCoordEmpty(y, h)) {
+              this.colourCell(y, h, '#1a1a1a');
+              this.colourCell(y + 1, h, this.COLOURS[this.funcArray[y][h]]);
+              this.funcArray[y + 1][h] = this.funcArray[y][h];
+              this.funcArray[y][h] = 0;
             }
           }
         }
-        ++k;
       }
-    }
+    }, 100);
   }
 
   checkMove(direction: string) {
@@ -187,8 +282,8 @@ export class TetrisComponent {
         for (let k = 0; k < 4; k++) {
           if (
             !this.isCoordEmpty(
-              this.currentBlockCoords[k][0] + 1,
-              this.currentBlockCoords[k][1]
+              this.CURRENT_BLOCK[k][0] + 1,
+              this.CURRENT_BLOCK[k][1]
             )
           ) {
             return true;
@@ -199,8 +294,8 @@ export class TetrisComponent {
         for (let k = 0; k < 4; k++) {
           if (
             !this.isCoordEmpty(
-              this.currentBlockCoords[k][0],
-              this.currentBlockCoords[k][1] + 1
+              this.CURRENT_BLOCK[k][0],
+              this.CURRENT_BLOCK[k][1] + 1
             )
           ) {
             return false;
@@ -211,8 +306,8 @@ export class TetrisComponent {
         for (let k = 0; k < 4; k++) {
           if (
             !this.isCoordEmpty(
-              this.currentBlockCoords[k][0],
-              this.currentBlockCoords[k][1] - 1
+              this.CURRENT_BLOCK[k][0],
+              this.CURRENT_BLOCK[k][1] - 1
             )
           ) {
             return false;
@@ -248,7 +343,28 @@ export class TetrisComponent {
 
       this.colourCell(Y, X, this.COLOURS[this.randomColor]);
 
-      this.currentBlockCoords[k] = [Y, X];
+      this.CURRENT_BLOCK[k] = [Y, X];
+    }
+  }
+
+  moveDown() {
+    for (let k = 0; k < 4; k++) {
+      let r = this.SHAPES[this.randomShape][k];
+      this.colourCell(
+        this.Y_AXIS - 1 < 0 ? this.Y_AXIS + r[0] : this.Y_AXIS + r[0],
+        r[1] + this.horz,
+        '#1a1a1a'
+      );
+    }
+    this.Y_AXIS++;
+    for (let k = 0; k < 4; k++) {
+      let r = this.SHAPES[this.randomShape][k];
+      let Y = this.Y_AXIS + r[0];
+      let X = r[1] + this.horz;
+
+      this.colourCell(Y, X, this.COLOURS[this.randomColor]);
+
+      this.CURRENT_BLOCK[k] = [Y, X];
     }
   }
 
@@ -297,7 +413,7 @@ export class TetrisComponent {
 
       this.colourCell(Y, X, this.COLOURS[this.randomColor]);
 
-      this.currentBlockCoords[k] = [Y, X];
+      this.CURRENT_BLOCK[k] = [Y, X];
     }
     // console.log(this.SHAPES[this.randomShape][5]);
   }
@@ -323,8 +439,9 @@ export class TetrisComponent {
         // Add your logic for Arrow Up key press here
         break;
       case 'ArrowDown':
-        console.log('Arrow Down key pressed');
-        // Add your logic for Arrow Down key press here
+        if (!this.checkMove('Down') && this.PEAK_HEIGHT != this.Y_AXIS) {
+          this.moveDown();
+        }
         break;
       case 'ArrowLeft':
         if (this.horz > 0 && this.checkMove('Left')) {
